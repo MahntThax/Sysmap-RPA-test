8d614829b4589056380f8c2bc8b7afb7baa67488
Gabriel Bertuol <mahntthaxarkyan@hotmail.com>
2023-11-28 16:58:23 -0300

    Base for the search function

---
+++ PageModels/UdemyMainPageModel.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Sysmap_udemy_test.PageModels
+{
+    internal class UdemyMainPageModel
+    {
+        private IWebElement udemyHeader;
+
+        private IWebElement udemyLogoButton;
+
+        private IWebElement searchBar;
+
+        private IWebElement searchButton;
+
+        public UdemyMainPageModel(IWebDriver webDriver)
+        {
+            // init elements in here
+        }
+
+        public void SearchByKeyword(string keyword)
+        {
+            if (string.IsNullOrWhiteSpace(keyword))
+            {
+                throw new ArgumentNullException("keyword");
+            }
+
+            if (searchBar == null)
+            {
+                throw new Exception("Udemy main page was not loaded properly");
+            }
+
+            searchBar.SendKeys(keyword);
+
+            searchButton.Click();
+        }
+    }
+}
