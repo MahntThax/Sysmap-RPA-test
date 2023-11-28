cf2131b6a3e3c16005d645ff69d6d51eddd92b95
Gabriel Bertuol <mahntthaxarkyan@hotmail.com>
2023-11-28 17:15:28 -0300

    Create basic selenium utilities

---
+++ Utilities/SeleniumUtilities.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+
+namespace Sysmap_udemy_test.Utilities
+{
+    /// <summary>
+    /// Class composed of methods to handle control of selenium actions
+    /// </summary>
+    internal static class SeleniumUtilities
+    {
+        public static bool DetectElementIsValid(IWebDriver driver, By elementSearchBy)
+        {
+            try
+            {
+                driver.FindElement(elementSearchBy);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static IWebElement ReturnElement(IWebDriver driver, By elementSearchBy)
+        {
+            try
+            {
+                return driver.FindElement(elementSearchBy);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
